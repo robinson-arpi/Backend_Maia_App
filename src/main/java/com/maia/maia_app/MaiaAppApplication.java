f269e6f088863360b1352c39670151336fd76749
robinson-arpi <robinson.arpi@gmail.com>
2024-04-20 19:57:41 -0500

    changes

--- src/main/java/com/maia/maia_app/MaiaAppApplication.java
+++ src/main/java/com/maia/maia_app/MaiaAppApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MaiaAppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MaiaAppApplication.class, args);
 	}
--- src/main/java/com/maia/maia_app/schedule/ScheduleRepository.java
+++ src/main/java/com/maia/maia_app/schedule/ScheduleRepository.java
@@ -18,12 +18,12 @@ public class ScheduleRepository {
     public List<ScheduleResponse> getUserSchedule(Integer userId, String day) {
         String sql = "SELECT h.schedule_id, c.subject_name, p.first_name AS ProfessorFirstName, p.last_name AS ProfessorLastName, " +
                 "a.classroom_name, h.day, bh.start_time, bh.end_time " +
-                "FROM Registrations r " +
-                "INNER JOIN Schedules h ON r.subject_id = h.subject_id " +
-                "INNER JOIN Subjects c ON h.subject_id = c.subject_id " +
-                "INNER JOIN Users p ON h.user_id = p.user_id " +
-                "INNER JOIN Classrooms a ON h.classroom_id = a.classroom_id " +
-                "INNER JOIN TimeBlocks bh ON h.block_id = bh.block_id " +
+                "FROM registrations r " +
+                "INNER JOIN schedules h ON r.subject_id = h.subject_id " +
+                "INNER JOIN subjects c ON h.subject_id = c.subject_id " +
+                "INNER JOIN users p ON h.user_id = p.user_id " +
+                "INNER JOIN classrooms a ON h.classroom_id = a.classroom_id " +
+                "INNER JOIN timeblocks bh ON h.block_id = bh.block_id " +
                 "WHERE r.user_id = ? AND h.day = ?";
 
         System.out.println("SQL: " + sql);
